50f4202e457a4e16a25d4e7eb1c2e17b80433d7a
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-05-04 16:38:10 -0600

    Actualizaciones

---
+++ open_academy/models/course.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Clases Base para Odoo
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+    _name = 'open_academy.course'
+    _description = 'Gestión de los Cursos Ofertados'
+    
+    name = fields.Char('Nombre', size=128, required=True)
+
+    # # Tipos # #
+    #  Char
+    #  Float
+    #  Boolean
+    #  Date
+    #  Datetime
+    #  Int
+    #  Text
+    #  Html
+    #  Binary
+
+    # # Relacionales
+    #  Many2one
+    #  One2many
+    #  Many2many
+    #  Related
+
+    # # Calculados
+    #  Function
