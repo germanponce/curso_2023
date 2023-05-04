8a9cc2815b7a9b27d02a28d6f42f2fffdc4db15b
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-05-04 16:03:13 -0600

    Actualizaciones

---
+++ open_academy/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Openacademy Curso 2023',
+    'version': '0.1',
+    'author': 'German Ponce Dominguez',
+    'website': 'http://poncesoft.blogspot.com',
+    'category': 'Capacitacion',
+    'summary': 'Desarrollo de formularios para capacitacion de desarrollo.',
+    'description': """
+
+Este modulo agrega una seria de formularios para entender el desarrollo de la API de Odoo V15
+
+    """,
+    'depends': ['base'],
+
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+    },
+    'license': 'LGPL-3',
+}
