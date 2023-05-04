727d1a338ed1643d5b77c6a5c69d74195e196e69
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-05-04 17:00:34 -0600

    Actualizaciones

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -7,8 +7,12 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'open_academy.course'
     _description = 'Gestión de los Cursos Ofertados'
-    
-    name = fields.Char('Nombre', size=128, required=True)
+    _order = 'date_start'
+
+    name = fields.Char('Nombre', size=128, required=True, index=True)
+
+    date_start = fields.Date('Fecha Inicio')
+    date_end = fields.Date('Fecha Fin')
 
     # # Tipos # #
     #  Char
