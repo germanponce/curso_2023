1b7c84431fb4da82968615122c31da33d0af8ea6
Germán Ponce Dominguez <german.ponce@argil.mx>
2023-05-11 16:39:21 -0600

    Actualizaciones

--- open_academy/models/res_partner.py
+++ open_academy/models/res_partner.py
@@ -5,11 +5,18 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
+    @api.depends('session_ids')
+    def _is_an_student(self):
+        for rec in self:
+            if rec.session_ids:
+                rec.is_student = True
+            else:
+                rec.is_student = False
 
     session_ids = fields.Many2many('open_academy.session', 
                                      'sessions_attendances_rel', 'partner_id', 'session_id', 
                                      'Sesiones Curso')
 
 
-    is_student = fields.Boolean('Es un estudiante')
-    
+    is_student = fields.Boolean('Es un estudiante', compute="_is_an_student", store=True)
+
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -22,6 +22,14 @@ class Session(models.Model):
     def _current_datetime(self):
         return fields.Datetime.now()
 
+    @api.depends('number_of_seats', 'attendace_ids')
+    def _get_takens_free(self):
+        for rec in self:
+            number_of_attendances = len(rec.attendace_ids)
+            number_of_seats_free = rec.number_of_seats - number_of_attendances
+            print ("#### number_of_seats_free: ", number_of_seats_free)
+            rec.number_of_seats_free = number_of_seats_free
+
     name = fields.Char('Descripción', size=128, required=True)
     initial_date = fields.Datetime('Fecha Inicio', default=_current_datetime)
     end_date = fields.Datetime('Fecha Fin')
@@ -41,6 +49,8 @@ class Session(models.Model):
     course_id = fields.Many2one('open_academy.course', 'Curso/ID Ref',
                                 help="Asocia la clase al curso...")
 
+    number_of_seats_free = fields.Integer('# Lugares Disponibles', compute="_get_takens_free")
+
     #### Query Consulta de Union de Tablas ####
     """ 
         select oas.name, rp.name nombre_sesion from open_academy_session as oas
@@ -49,3 +59,24 @@ class Session(models.Model):
                                    join res_partner as rp
                                      on rp.id = sar.partner_id
     """
+
+
+    @api.onchange('number_of_seats', 'attendace_ids')
+    def onchange_number_of_seats(self):
+        if self.number_of_seats and self.attendace_ids:
+            number_of_attendances = len(self.attendace_ids)
+            number_of_seats_free = self.number_of_seats - number_of_attendances
+            print ("########### number_of_seats_free:", number_of_seats_free)
+            if number_of_seats_free < 0:
+                number_of_attendances = self.number_of_seats + abs(number_of_seats_free)
+                print ("########### number_of_attendances:", number_of_attendances)
+                self.number_of_seats = number_of_attendances
+                return {
+                            'warning': {
+                                'title': "Advertencia!!!!",
+                                'message': "El no. de lugares no puede ser un valor negativo.\n\
+El no. de lugares se ha actualizado para cubrir el no. de asientos necesarios.\n\
+Asientos Faltantes: %s" % abs(number_of_seats_free),
+                            },
+                        }
+    
